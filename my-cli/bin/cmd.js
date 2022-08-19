40dc1a76107ee4e39e5e7c40fc9daeaf7f38bedb
Simon Cateau <sorani.dev@outlook.com>
2022-08-19 09:03:59 +0200

    fix: Empty args._ elements populated

--- my-cli/bin/cmd.js
+++ my-cli/bin/cmd.js
@@ -1,29 +1,51 @@
 #!/usr/bin/env node
 
 const got = require('got')
+const minimist = require('minimist')
 
 const API = 'http://localhost:3000'
 
-const usage = (msg = 'Back office for My App') => {   console.log(`\n${msg}\n`, ' usage: my-cli <id> <amount>') }
+const usage = (msg = 'Back office for My App') => {
+  console.log(`\n${msg}\n`,
+    '     usage: my-cli <id> --amount=<int> --api=<string>\n',
+    '            my-cli <id> -n=<int> --api=<string>\n')
+}
 
 const argv = process.argv.slice(2)
-if (argv.length < 2) {
+const args = minimist(argv, {
+  alias: { amount: 'n' },
+  string: ['api'],
+  default: { api: API }
+})
+
+if (args._.length < 1) {
   usage()
   process.exit(1)
 }
-const [ id, amt ] = argv
-const amount = Number(amt)
-console.log(argv, id, amt, amount)
+console.log(args)
+// Remove empty elements from the args._ array
+// Code from https://bobbyhadz.com/blog/javascript-remove-empty-elements-from-array
+args._ = args._.filter(element => (
+    element !== null &&
+    element !== undefined &&
+    element !== '' &&
+    !Number.isNaN(element)
+))
+console.log(args)
+const [ id ] = args._
+const { amount, api } = args
+console.log("!", id, amount, api)
 if (Number.isInteger(amount) === false) {
-  usage('Error: amount must be an integer')
+  usage('Error: --amount flag is required and must be an integer')
   process.exit(1)
 }
 
 (async() => {
 
   try {
-    const url = `${API}/orders/${id}`
-		console.log(url)
+    const url = `${api}/orders/${id}`
+	  console.log(url)
+	console.log(amount)
     await got.post(url, {
       json: { amount }
     })
@@ -31,4 +53,5 @@ if (Number.isInteger(amount) === false) {
     console.error(err.message)
     process.exit(1)
   }
+
 })()
